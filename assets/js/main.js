d14c2e3b8ecda28eb15982da725646be3b149349
Elxokas2 <106853974+Elxokas2@users.noreply.github.com>
2023-12-08 23:10:56 +0100

    Commit initial

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const body = document.querySelector("body");
+    document.body.style.backgroundImage = "url('assets/img/hero.jpg')";
+    body.style.backgroundSize = "cover";
+});
